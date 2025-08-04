09c4e3f72ebef4a9c1a6ba93164bf69669c61d9c
Juan Silva <162917944+juancapanegra@users.noreply.github.com>
2025-08-04 21:48:13 +0000

    Completed: Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon',];
+
+for (let i = 0; i < pronoun.length; i++) {
+
+  for (let j = 0; j < adj.length; j++) {
+
+    for (let k = 0; k < noun.length; k++) {
+
+      for (i = 0; i < pronoun.length; i++) {
+
+        for (j = 0; j < adj.length; j++) {
+
+          for (k = 0; k < noun.length; k++) {
+            console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+            console.log(pronoun[i] + adj[j] + noun[k] + '.org');
+            console.log(pronoun[i] + adj[j] + noun[k] + '.net');
+            console.log(pronoun[i] + adj[j] + noun[k] + '.io');
+            console.log(pronoun[i] + adj[j] + noun[k] + '.info');
+            console.log(pronoun[i] + adj[j] + noun[k] + '.us');
+
+          }
+        }
+      }
+    }
+  }
+}  
