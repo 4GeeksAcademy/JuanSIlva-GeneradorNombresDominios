3167a674ba33ce9e00b8d023e51b254b98fe4741
Juan Silva <162917944+juancapanegra@users.noreply.github.com>
2025-08-06 18:57:28 +0000

    Refactorizadción: Generador-Dominios

--- src/app.js
+++ src/app.js
@@ -13,28 +13,54 @@ window.onload = function () {
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon',];
+let extensions = ['.com', '.org', '.net', '.io', '.info', '.us'];
+
+//Código anterior (comentado):
+
+//for (let i = 0; i < pronoun.length; i++) {
+
+//for (let j = 0; j < adj.length; j++) {
+
+//for (let k = 0; k < noun.length; k++) {
+
+//for (i = 0; i < pronoun.length; i++) {
+
+//for (j = 0; j < adj.length; j++) {
+
+//for (k = 0; k < noun.length; k++) {
+//console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+//console.log(pronoun[i] + adj[j] + noun[k] + '.org');
+//console.log(pronoun[i] + adj[j] + noun[k] + '.net');
+//console.log(pronoun[i] + adj[j] + noun[k] + '.io');
+//console.log(pronoun[i] + adj[j] + noun[k] + '.info');
+//console.log(pronoun[i] + adj[j] + noun[k] + '.us');
+
+//}
+//}
+//}
+//}
+//}
+//}  
+
+
+// Código corregido:
+
+// 1.Se eliminó las líneas en blanco de los primeros dos "for"
+
+// 2.Se eliminaron los ultimos tres "for" ya que no eran necesarios, 
+//    porque se llegaba al mismo resultado.
+
+// 3.Los "console.log" que mostraban las multiples extensiones 
+//    se sustituyeron por un "for" que itera e imprime en pantalla
+//    las extensiones de un nuevo array: extensions
+
 
 for (let i = 0; i < pronoun.length; i++) {
-
   for (let j = 0; j < adj.length; j++) {
-
     for (let k = 0; k < noun.length; k++) {
-
-      for (i = 0; i < pronoun.length; i++) {
-
-        for (j = 0; j < adj.length; j++) {
-
-          for (k = 0; k < noun.length; k++) {
-            console.log(pronoun[i] + adj[j] + noun[k] + '.com');
-            console.log(pronoun[i] + adj[j] + noun[k] + '.org');
-            console.log(pronoun[i] + adj[j] + noun[k] + '.net');
-            console.log(pronoun[i] + adj[j] + noun[k] + '.io');
-            console.log(pronoun[i] + adj[j] + noun[k] + '.info');
-            console.log(pronoun[i] + adj[j] + noun[k] + '.us');
-
-          }
-        }
+      for (let numeroExt = 0; numeroExt < extensions.length; numeroExt++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extensions[numeroExt]);
       }
     }
   }
-}  
+}
